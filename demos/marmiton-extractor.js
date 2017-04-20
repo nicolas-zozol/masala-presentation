f48cd8955aac8d446caf5fb39b0cd5d62d53f805
Nicolas Zozol <nzozol@gmail.com>
2017-04-20 14:15:01 +0200

    Marmiton-Extractor working correctly

--- demos/marmiton-extractor.js
+++ demos/marmiton-extractor.js
@@ -1,35 +1,51 @@
-const {F, C,N, X, T} = require('parser-combinator');
+const {F, C, N, X, T} = require('parser-combinator');
+const lineStop = C.string('- ');
+let x = new X();
 
 
-const html = ()=> C.char('[').then(C.notChar(']')).then(C.char(']'));
-
-const x = new X({
-    wordSeparators: html
-});
-
-
-function quantity(){
-    return x.word();
+function quantity() {
+    return N.integer;
 }
 
-function unit(){
-    const knownUnits=['cuillère à café', 'g','l', 'ml', 'cuillère', 'cuillères']
+function unit() {
+    const knownUnits = ['cuillère à café', 'g', 'l', 'ml', 'cuillère', 'cuillères']
+    
+    const combinator = T.blank()
+        .thenRight(x.stringIn(knownUnits))
+        .thenLeft(T.blank());
 
 
-    const combinator= T.blank().thenRight(x.stringIn(knownUnits)).thenLeft(T.blank());
-
-    return combinator;
-    //return combinator.opt().map(x=>x.value);
+    return combinator.opt().map(v=>v.value);
 
 }
 
-function ingredient(){
-    const combinator= F.not(C.string('- ')).rep().map(c=>c.join(''));
+function ingredient() {
+    const html = C.char('[')
+        .then(C.notChar(']').rep())
+        .then(C.char(']'))
+        .debug('HTML');
+
+    const ignore = html.or(T.eol).map(c=>'');
+
+    //let combinator= x.words().rep();
+    const word = F.try(ignore).or(C.notString('- ')).rep().map(c=>c.join(''));
+
+    let combinator = word;
+
     return combinator;
 
 }
 
 function structuredLine(){
-    return C.string('- ')
+    return lineStop
         .thenRight( quantity().then(unit()).then(ingredient()));
+}
+
+function marmitonCombinator() {
+    return x.wordsUntil(lineStop).thenRight(structuredLine().rep());
+}
+
+
+module.exports = {
+    marmitonCombinator
 }
